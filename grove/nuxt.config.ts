76e45c6c8541851985cc84f35179a59a29e606be
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-02-18 11:30:58 +0100

    create nuxt app

---
+++ grove/nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxtjs/device',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/seo',
+    '@pinia/nuxt'
+  ]
+})
