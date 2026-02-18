1c2b8221118ded2d43a59327edd1f1af15d838ba
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-02-18 11:39:36 +0100

    add website homepage

--- grove/nuxt.config.ts
+++ grove/nuxt.config.ts
@@ -3,6 +3,32 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  css: ['~/assets/css/main.css'],
+
+  colorMode: {
+    preference: 'light',
+    fallback: 'dark',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storage: 'localStorage',
+    storageKey: 'nuxt-color-mode',
+  },
+
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap',
+        },
+      ],
+    },
+  },
+
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
@@ -12,6 +38,6 @@ export default defineNuxtConfig({
     '@nuxtjs/device',
     '@nuxtjs/eslint-module',
     '@nuxtjs/seo',
-    '@pinia/nuxt'
-  ]
+    '@pinia/nuxt',
+  ],
 })
