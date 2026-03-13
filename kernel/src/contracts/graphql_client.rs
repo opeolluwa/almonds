586fb6ea9bbef9a468927d6d4fc3f083b3cd0e51
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-03-13 16:20:32 +0100

    attempt setting up grapql

---
+++ kernel/src/contracts/graphql_client.rs
@@ -0,0 +1,13 @@
+use graphql_client::GraphQLQuery;
+
+
+
+// The paths are relative to the directory where your `Cargo.toml` is located.
+// Both json and the GraphQL schema language are supported as sources for the schema
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = ".graphql/sync_queue_schema.graphql",
+    query_path = ".graphql/sync_queue_query.graphql",
+)]
+pub struct SyncQueueView;
+
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -4,7 +4,7 @@ pub mod error;
 pub mod kernel;
 pub mod repositories;
 pub mod utils;
-
+pub mod contracts;
 pub use graphql_ws_client::*;
 pub use sea_orm;
 
