00d7b80341d0d9aef58a35303c0240fd763dc6b6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-03-22 23:15:53 +0100

    update: add reusable component to wrap around transfer and duplicate

--- almonds/app/components/meta/meta-controls.vue
+++ almonds/app/components/meta/meta-controls.vue
@@ -1,18 +1,13 @@
 <template>
-<UPopover mode="hover">
+  <UPopover mode="hover">
     <UIcon
       name="heroicons:ellipsis-vertical"
       class="size-5 hover:text-accent-500 shrink-0"
     />
-  <template #content>
-
-    <Placeholder class="size-48 m-4 inline-flex" />
-  </template>
-</UPopover>
+    <template #content>
+      <Placeholder class="size-48 m-4 inline-flex" />
+    </template>
+  </UPopover>
 </template>
 
-
-<script lang="ts" setup>
-
-
-</script>
+<script lang="ts" setup></script>
