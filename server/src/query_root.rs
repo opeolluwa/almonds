a1ed055203b7c6126071440998a8d3a8938cad48
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-04-17 12:44:44 +0100

    update

--- server/src/query_root.rs
+++ server/src/query_root.rs
@@ -1,5 +1,4 @@
-// use almond_kernel::entities::register_active_enums;
-// use almond_kernel::entities::register_entity_modules;
+use almond_kernel::entities::{register_active_enums, register_entity_modules};
 use async_graphql::dynamic::*;
 use sea_orm::DatabaseConnection;
 use seaography::{async_graphql, lazy_static::lazy_static, Builder, BuilderContext};
@@ -25,8 +24,8 @@ pub fn schema_builder(
     complexity: Option<usize>,
 ) -> SchemaBuilder {
     let mut builder = Builder::new(context, database.clone());
-    // builder = register_entity_modules(builder);
-    // builder = register_active_enums(builder);
+    builder = register_entity_modules(builder);
+    builder = register_active_enums(builder);
 
     // seaography::register_custom_inputs!(
     //     builder,
